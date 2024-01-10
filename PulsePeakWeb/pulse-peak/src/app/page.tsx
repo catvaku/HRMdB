4253384e7ceb3938b6839a25360939fdce2ef12c
v4run.tomar@gmail.com <v4run.tomar@gmail.com>
2024-01-10 15:17:45 +0530

    "Update page.tsx to display Pulse Peak UI with a red heart icon of size 50."

--- PulsePeakWeb/pulse-peak/src/app/page.tsx
+++ PulsePeakWeb/pulse-peak/src/app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="flex border-violet-500 p-4 items-center gap-4">
-        <p className="text-lg">Pulse Peak UI</p>
+        <p className="text-4xl">Pulse Peak UI</p>
         <Heart
           color="red"
-          size={20}
+          size={50}
           fill="red"
           className="animate-ping animate-pulse"
         ></Heart>
